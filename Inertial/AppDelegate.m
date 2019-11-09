63d291f4aa85ccbab7b13fca18adb757f7c28fa3
Xiao Jie <xiaojie@basecity.com>
2019-11-09 11:55:46 +0800

    惯性支持中断

--- Inertial/AppDelegate.m
+++ Inertial/AppDelegate.m
@@ -19,7 +19,7 @@ NSTimer *timer;
 CGEventTimestamp timestamp;
 NSTimeInterval interval;
 const double inertialDuration = 0.6; //  惯性持续时间，以秒为单位
-const double speedThreshold = 0.5; // 移动速度门槛
+const double speedThreshold = 0.8; // 移动速度门槛
 const double inertialPower = 3.0; // 幂
 const int magnify = 30; // 惯性倍数
 
@@ -32,16 +32,20 @@ CGEventTimestamp lastTime, currentTime;
 double speed;
 
 bool enable;
+bool autoInertial;
+bool inertialBreak;
 
 
 // 构造函数
 -(id) init {
     if(self = [super init]) {
         enable = true;
+        autoInertial = false;
+        inertialBreak = false;
         lastPoint = currentPoint = CGPointZero;
         lastTime =  currentTime = 0;
         speed = 0;
-        interval = 0.03; // 定时器间隔
+        interval = 1.0 / 60; // 定时器间隔
 
         timer = [NSTimer scheduledTimerWithTimeInterval:interval repeats:YES block:^(NSTimer * _Nonnull timer) {
             
@@ -55,6 +59,7 @@ bool enable;
                 if(moveTimestamp == currentTime)
                 {
                     mouseStatus = 1; //停止状态
+                    inertialBreak = false;
                 }
                 else
                 {
@@ -98,6 +103,16 @@ bool enable;
                 
                 if(mouseStatus == 3)
                 {
+                    if(inertialBreak == true){
+//                        NSLog(@"惯性被中断");
+                        mouseStatus = 4; // 惯性移动结束
+                        inertialCount = 0; // 惯性移动次数清 0
+                        autoInertial = false;
+                        return ;
+                    }
+                    
+                    
+                    autoInertial = true;
                     inertialCount ++;
                     CGPoint target;
                     double temp = pow(fabs(inertialCount * timer.timeInterval - inertialDuration), inertialPower);
@@ -111,6 +126,7 @@ bool enable;
                     {
 //                        NSLog(@"惯性结束");
                         mouseStatus = 4; // 惯性移动结束
+                        autoInertial = false;
                         inertialCount = 0; // 惯性移动次数清 0
                     }
                 }
@@ -146,21 +162,23 @@ bool enable;
 
 static CGEventRef eventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
 
-	CGPoint eventLocation = CGEventGetLocation(event);
     lastPoint = currentPoint;
-    currentPoint = eventLocation;
-    CGEventTimestamp eventTimestamp = CGEventGetTimestamp(event);// 启动后的纳秒计数
-    timestamp = eventTimestamp;
+    currentPoint = CGEventGetLocation(event);;
     lastTime =  currentTime;
-    currentTime = timestamp;
+    currentTime = CGEventGetTimestamp(event);// 启动后的纳秒计数
     double offsetTime = (currentTime - lastTime) / 1000000.0;
     double offsetPowX = pow(currentPoint.x - lastPoint.x, 2);
     double offsetPowY = pow(currentPoint.y - lastPoint.y, 2);
+    // 三角形勾股定理求斜边
     speed = sqrt(offsetPowX + offsetPowY) / offsetTime;
     
 //    NSLog(@"移动速度：%f px/ms", speed);
     if(mouseStatus != 3)
         mouseStatus = 0; // 常规移动
+    if(autoInertial == true) // 光标自动惯性移动
+        autoInertial = false;
+    else if(mouseStatus == 3)
+        inertialBreak = true;
     return event;
 }
 
