eb092ae5c2467be885988f77e59e35fda6041a86
Xiao Jie <xiaojie@basecity.com>
2019-11-10 10:52:32 +0800

    调整变量命名

--- Inertial/AppDelegate.m
+++ Inertial/AppDelegate.m
@@ -14,79 +14,85 @@
 
 @implementation AppDelegate
 
-// 私有变量
-NSTimer *timer;
-CGEventTimestamp timestamp;
-NSTimeInterval interval;
+#define INERTIAL_COUNT_ZERO 0
+
+typedef CF_ENUM(int, INERTIAL_MOUSE_STATUS) {
+	INERTIAL_MOUSE_STATUS_UNKNOW = 0,
+	INERTIAL_MOUSE_STATUS_STOP = 1,
+	INERTIAL_MOUSE_STATUS_INERTIAL_START = 2,
+	INERTIAL_MOUSE_STATUS_INERTIAL_MOVE = 3,
+	INERTIAL_MOUSE_STATUS_INERTIAL_STOP = 4
+};
+
+// 常量
 const double inertialDuration = 0.6; //  惯性持续时间，以秒为单位
 const double speedThreshold = 0.8; // 移动速度门槛
 const double inertialPower = 3.0; // 幂
 const int magnify = 30; // 惯性倍数
+const NSTimeInterval interval = 1.0 / 60.0; // 定时器间隔
 
-int mouseStatus = 0;
-
-CGPoint pointFinish;
-
-CGPoint lastPoint, currentPoint;
-CGEventTimestamp lastTime, currentTime;
-double speed;
-
-bool enable;
+// 私有变量
+NSTimer *timer = NULL;
+int mouseStatus = INERTIAL_MOUSE_STATUS_UNKNOW;
+int inertialCount = INERTIAL_COUNT_ZERO;
+CGPoint inertialStartPoint, lastPoint, currentPoint;
+CGEventTimestamp lastTimestamp, currentTimestamp;
+double mouseMoveSpeed;
+bool enableInertial;
 bool autoInertial;
 bool inertialBreak;
+
+
+CGEventTimestamp moveTimestamp = 0;
+double inertialOffsetX = 0.0; // 惯性移动的水平位移
+double inertialOffsetY = 0.0; // 惯性移动的垂直位移
 
 
 // 构造函数
 -(id) init {
     if(self = [super init]) {
-        enable = true;
+        enableInertial = true;
         autoInertial = false;
         inertialBreak = false;
         lastPoint = currentPoint = CGPointZero;
-        lastTime =  currentTime = 0;
-        speed = 0;
-        interval = 1.0 / 60; // 定时器间隔
+        lastTimestamp =  currentTimestamp = 0;
+        mouseMoveSpeed = 0;
 
         timer = [NSTimer scheduledTimerWithTimeInterval:interval repeats:YES block:^(NSTimer * _Nonnull timer) {
             
-            static CGEventTimestamp moveTimestamp = 0;
-            static double offsetX = 0.0;
-            static double offsetY = 0.0;
-            
             // 判断鼠标当前状态
-            if(mouseStatus == 0)
+            if(mouseStatus == INERTIAL_MOUSE_STATUS_UNKNOW)
             {
-                if(moveTimestamp == currentTime)
+                if(moveTimestamp == currentTimestamp)
                 {
-                    mouseStatus = 1; //停止状态
+                    mouseStatus = INERTIAL_MOUSE_STATUS_STOP; //停止状态
                     inertialBreak = false;
                 }
                 else
                 {
-                    moveTimestamp = currentTime;
+                    moveTimestamp = currentTimestamp;
                     return;
                 }
             }
             
-            if(enable == true)
+            if(enableInertial == true)
             {
-                if(mouseStatus == 1)
+                if(mouseStatus == INERTIAL_MOUSE_STATUS_STOP)
                 {
-                    if(speed > speedThreshold)
+                    if(mouseMoveSpeed > speedThreshold)
                     {
-                        mouseStatus = 2; // 鼠标处于惯性状态
-                        pointFinish = currentPoint; // 记下结束时的坐标
+                        mouseStatus = INERTIAL_MOUSE_STATUS_INERTIAL_START; // 鼠标处于惯性状态
+                        inertialStartPoint = currentPoint; // 记下结束时的坐标
                     }
                 }
                 
-                static int inertialCount = 0;
                 static double aX = 0.0, aY = 0.0;
-                if(mouseStatus == 2)
+                if(mouseStatus == INERTIAL_MOUSE_STATUS_INERTIAL_START)
                 {
 //                    NSLog(@"正在计算惯性属性");
                     // 计算出惯性将要移动的偏移量
-                    offsetX = magnify * (currentPoint.x - lastPoint.x);
-                    offsetY = magnify * (currentPoint.y - lastPoint.y);
+                    inertialOffsetX = magnify * (currentPoint.x - lastPoint.x);
+                    inertialOffsetY = magnify * (currentPoint.y - lastPoint.y);
                     
                     // 使用抛物线 y=a((x+b)^n)
                     // 持续 500 ms， b = -0.5
@@ -95,18 +101,18 @@ bool inertialBreak;
                     // 起点时， x = 0， a*b^2 = 变化总区间
                     // a*(-500)^2 = distance
                     double inertialDurationPow = pow(inertialDuration, inertialPower);
-                    aX = offsetX / inertialDurationPow;
-                    aY = offsetY / inertialDurationPow;
-                    mouseStatus = 3;
-                    inertialCount = 0;
+                    aX = inertialOffsetX / inertialDurationPow;
+                    aY = inertialOffsetY / inertialDurationPow;
+                    mouseStatus = INERTIAL_MOUSE_STATUS_INERTIAL_MOVE;
+                    inertialCount = INERTIAL_COUNT_ZERO;
                 }
                 
-                if(mouseStatus == 3)
+                if(mouseStatus == INERTIAL_MOUSE_STATUS_INERTIAL_MOVE)
                 {
                     if(inertialBreak == true){
 //                        NSLog(@"惯性被中断");
-                        mouseStatus = 4; // 惯性移动结束
-                        inertialCount = 0; // 惯性移动次数清 0
+                        mouseStatus = INERTIAL_MOUSE_STATUS_INERTIAL_STOP; // 惯性移动结束
+                        inertialCount = INERTIAL_COUNT_ZERO; // 惯性移动次数清 0
                         autoInertial = false;
                         return ;
                     }
@@ -116,8 +122,8 @@ bool inertialBreak;
                     inertialCount ++;
                     CGPoint target;
                     double temp = pow(fabs(inertialCount * timer.timeInterval - inertialDuration), inertialPower);
-                    target.x = pointFinish.x + offsetX - (aX * temp);
-                    target.y = pointFinish.y + offsetY - (aY * temp);
+                    target.x = inertialStartPoint.x + inertialOffsetX - (aX * temp);
+                    target.y = inertialStartPoint.y + inertialOffsetY - (aY * temp);
                     
 //                    NSLog(@"触发惯性移动第%d次, [%f, %f]", inertialCount, target.x, target.y);
                     [self PostMouseEventButton:kCGMouseButtonLeft Type: kCGEventMouseMoved Point: &target Count:1];
@@ -125,9 +131,9 @@ bool inertialBreak;
                     if(inertialCount * timer.timeInterval >= inertialDuration)
                     {
 //                        NSLog(@"惯性结束");
-                        mouseStatus = 4; // 惯性移动结束
+                        mouseStatus = INERTIAL_MOUSE_STATUS_INERTIAL_STOP; // 惯性移动结束
                         autoInertial = false;
-                        inertialCount = 0; // 惯性移动次数清 0
+                        inertialCount = INERTIAL_COUNT_ZERO; // 惯性移动次数清 0
                     }
                 }
             }
@@ -164,17 +170,17 @@ static CGEventRef eventCallback(CGEventTapProxy proxy, CGEventType type, CGEvent
 
     lastPoint = currentPoint;
     currentPoint = CGEventGetLocation(event);;
-    lastTime =  currentTime;
-    currentTime = CGEventGetTimestamp(event);// 启动后的纳秒计数
-    double offsetTime = (currentTime - lastTime) / 1000000.0;
+    lastTimestamp =  currentTimestamp;
+    currentTimestamp = CGEventGetTimestamp(event);// 启动后的纳秒计数
+    double offsetTime = (currentTimestamp - lastTimestamp) / 1000000.0;
     double offsetPowX = pow(currentPoint.x - lastPoint.x, 2);
     double offsetPowY = pow(currentPoint.y - lastPoint.y, 2);
     // 三角形勾股定理求斜边
-    speed = sqrt(offsetPowX + offsetPowY) / offsetTime;
+    mouseMoveSpeed = sqrt(offsetPowX + offsetPowY) / offsetTime;
     
 //    NSLog(@"移动速度：%f px/ms", speed);
-    if(mouseStatus != 3)
-        mouseStatus = 0; // 常规移动
+    if(mouseStatus != INERTIAL_MOUSE_STATUS_INERTIAL_MOVE)
+        mouseStatus = INERTIAL_MOUSE_STATUS_UNKNOW; // 常规移动
     if(autoInertial == true) // 光标自动惯性移动
         autoInertial = false;
     else if(mouseStatus == 3)
